093339604f54167e66123d0f38e2007a70173c49
yvonne <lillian1130@163.com>
2022-03-25 01:01:51 +0800

    gitee commit

--- testgit/src/test.java
+++ testgit/src/test.java
@@ -6,5 +6,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("git push test");
         System.out.println("git pull test");
+        System.out.println("gitee pull test");
     }
 }
